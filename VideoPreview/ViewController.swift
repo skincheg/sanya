3fa20f31eb44886df7ed167d570942ea5c6dacc0
bnkwsr5 <bnkwsr5@mail.ru>
2020-09-13 18:46:18 +0500

    Initial Commit

---
+++ VideoPreview/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VideoPreview
+//
+//  Created by bnkwsr5 on 13.09.2020.
+//  Copyright © 2020 bnkwsr5. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
